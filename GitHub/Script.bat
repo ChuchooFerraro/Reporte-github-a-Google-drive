47f8464aff51049226e75977ca2101aca8b2c704
ChuchooFerraro <86579315+ChuchooFerraro@users.noreply.github.com>
2022-09-19 11:24:44 -0300

    Update Script.bat

--- GitHub/Script.bat
+++ GitHub/Script.bat
@@ -1,5 +1,5 @@
 cd C:\Users\guido\Documents\GitHub
-gh issue list --repo drixit/product-issue-reporting --state all --limit 1000 > issues.csv
+gh issue list --repo "REPO NAME" --state all --limit 1000 > issues.csv
 
 
 cd C:\Users\guido\Documents\GitHub
